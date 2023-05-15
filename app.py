b6f521717bf237d8f159e4e1ce77fc69fe026e12
1abhi6 <1abhigup6@gmail.com>
2023-05-15 20:35:12 +0530

    Add get /store endpoint

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "my item",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+
+@app.get('/store')
+def get_stores():
+    return {'stores':stores}
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
