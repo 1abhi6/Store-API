f9c3ab15e918703c0a9498e35ec53366b0df77f5
1abhi6 <1abhigup6@gmail.com>
2023-05-18 16:23:44 +0530

    Add blueprint to structure the code

---
+++ resources/store.py
@@ -0,0 +1,58 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import stores
+
+blp = Blueprint("Stores", __name__, description="Operaations on stores")
+
+
+@blp.route('/store')
+class StoreList(MethodView):
+    # Get all stores
+    def get(self):
+        return {
+            'stores': list(stores.values())
+        }
+
+    # Create store
+    def post(self):
+        store_data = request.get_json()
+        if 'name' not in store_data:
+            abort(
+                400,
+                message='Bad request. Ensure "name" is included in the JSON payload'
+            )
+        for store in stores.values():
+            if store_data['name'] == store['name']:
+                abort(
+                    400,
+                    message='Store already exists.'
+                )
+        store_id = uuid.uuid4().hex
+        store = {
+            **store_data,
+            'id': store_id
+        }
+        stores[store_id] = store
+        return store, 201
+
+
+@blp.route('/store/<string:store_id>')
+class Store(MethodView):
+    # Get a store
+    def get(self, store_id):
+        try:
+            return stores[store_id]
+        except KeyError:
+            abort(404, message='Store not found.')
+
+    # Delete a store
+    def delete(self, store_id):
+        try:
+            del stores[store_id]
+            return {
+                'message': 'Store deleted.'
+            }
+        except:
+            abort(404, message='Store not found.')
