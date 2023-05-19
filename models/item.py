c02d793587bc78485bd7f2ac9226175f12974686
1abhi6 <1abhigup6@gmail.com>
2023-05-19 18:46:06 +0530

    Add one-to-many relationship with SQLAlchemy

--- models/item.py
+++ models/item.py
@@ -7,4 +7,7 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
     price = db.Column(db.Float(precision=2), nullable=False, unique=False)
-    store_id = db.Column(db.Integer, unique=False, nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey(
+        'stores.id'), unique=False, nullable=False)
+    
+    store = db.relationship('StoreModel', back_populates='items')
